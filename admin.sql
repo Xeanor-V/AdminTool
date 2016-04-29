faf4bf547b6305683df5194462b355125bcd20e0
Edgar Augusto Santiago Nieves <garo.edgar21@gmail.com>
2016-04-28 21:01:50 -0500

    Error en la base de datos, esta corregido

--- admin.sql
+++ admin.sql
@@ -1,41 +1,44 @@
-create database adminproyecto;
+create database adminproyecto12;
 
-CREATE TABLE `adminproyecto`.`usuario` (
+CREATE TABLE `adminproyecto12`.`usuario` (
   `idusuario` INT NOT NULL AUTO_INCREMENT,
   `nombre` VARCHAR(45) NOT NULL,
   `correo` VARCHAR(45) NOT NULL,
   `contra` VARCHAR(45) NOT NULL,
   `nocuenta` VARCHAR(20) NOT NULL,
+  UNIQUE(`nocuenta`),
   PRIMARY KEY (`idusuario`));
 
-CREATE TABLE `adminproyecto`.`registro` (
+CREATE TABLE `adminproyecto12`.`registro` (
   `idregistro` INT NOT NULL AUTO_INCREMENT,
   `nombre` VARCHAR(45) NOT NULL,
   `idusuario` INT NOT NULL,
   PRIMARY KEY (`idregistro`),
+  UNIQUE(`nombre`),
   INDEX `idusuario_idx` (`idusuario` ASC),
   CONSTRAINT `idusuario`
     FOREIGN KEY (`idusuario`)
-    REFERENCES `adminproyecto`.`usuario` (`idusuario`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    REFERENCES `adminproyecto12`.`usuario` (`idusuario`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE);
 
-CREATE TABLE `adminproyecto`.`cuenta` (
+CREATE TABLE `adminproyecto12`.`cuenta` (
   `idcuenta` INT NOT NULL AUTO_INCREMENT,
   `nombre` VARCHAR(45) NOT NULL,
   `tipo` INT NOT NULL,
   `idregistro` INT NOT NULL,
+  `subtipo` INT,
+  `identificador` VARCHAR(45),
   PRIMARY KEY (`idcuenta`),
   INDEX `idregistro_idx` (`idregistro` ASC),
   CONSTRAINT `idregistro`
     FOREIGN KEY (`idregistro`)
-    REFERENCES `adminproyecto`.`registro` (`idregistro`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    REFERENCES `adminproyecto12`.`registro` (`idregistro`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE);
 
-CREATE TABLE `adminproyecto`.`operacion` (
+CREATE TABLE `adminproyecto12`.`operacion` (
   `idoperacion` INT NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(45) NOT NULL,
   `cantidad` INT NOT NULL,
   `tipo` INT NOT NULL,
   `idcuenta` INT NOT NULL,
@@ -43,6 +46,13 @@ CREATE TABLE `adminproyecto`.`operacion` (
   INDEX `idcuenta_idx` (`idcuenta` ASC),
   CONSTRAINT `idcuenta`
     FOREIGN KEY (`idcuenta`)
-    REFERENCES `adminproyecto`.`cuenta` (`idcuenta`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    REFERENCES `adminproyecto12`.`cuenta` (`idcuenta`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE);
+
+insert into usuario(nombre, correo, contra, nocuenta) values('Edgar', 'garo.edgar21@gmail.com', 'edgar21', '2014330415');
+insert into registro(nombre, idusuario) value('Cocodrilo', 2);
+insert into registro(nombre, idusuario) value('Cocodrilo2', 2);
+insert into registro(nombre, idusuario) value('Cocodrilo3', 2);
+insert into cuenta(nombre, tipo, idregistro, subtipo, identificador) values('banco', 1, 2, 1, 10);
+
